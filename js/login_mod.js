47abd00263d901cfba07c36ebfc6c79d462f7c26
MJG <MJG@MacBook-Pro.local>
2016-04-04 19:14:54 -0400

    login mod is able to retrieve user data from github

--- js/login_mod.js
+++ js/login_mod.js
@@ -1,4 +1,26 @@
-(function() {
+(function(ns) {
   'use strict';
-  
-})();
+
+  $('#login-form').on('submit', function githubLogin(event) {
+    event.preventDefault();
+    var enteredToken = $(this).find('#token-entry').val();
+    ajaxLogin(enteredToken);
+  });
+
+  function ajaxLogin(enteredToken) {
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.github.com/user',
+      dataType: 'JSON',
+      headers: {Authorization: "token " + enteredToken},
+      success: function tokenAcquired(data) {
+        console.log(data);
+      },
+      error: function tokenNotAcquired(xhr) {
+        console.log(xhr);
+      }
+    });
+  }
+
+  window.ns = ns;
+})(window.ns || {});
