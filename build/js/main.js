eafe77a5b42ea933509ba708d3e99ec4b0482a64
MJG <MJG@MacBook-Pro.local>
2016-04-08 16:19:41 -0400

    tabs are being added mostly

--- build/js/main.js
+++ build/js/main.js
@@ -55,17 +55,16 @@
     view = (view || "");
     var viewElement = $(view);
     var modName = view.substr(1);
-    console.log(modName);
-    console.log(modName.substr(0, 11));
     if(ns[modName] && ns[modName].load){
       ns[modName].load();
     } else if (modName.substr(0, 5) === 'repo-'){
       ns.repoDetails.load(modName.substr(5), function detailsCallback(element) {
+        ns._addTabs('repodetail', view);
         element.show();
       });
     } else if (modName.substr(0, 11) === 'repoissues-'){
-      console.log('triggering repoissues- function');
       ns.repoIssues.load(modName.substr(11), function issuesCallback(element) {
+        ns._addTabs('repoissue', view);
         element.show();
       });
 
@@ -75,6 +74,43 @@
     }
     viewElement.show();
   };
+
+  ns._addTabs = function addTabs(type, hash) {
+    if(type === 'repodetail') {
+      $('#temp-tags').empty();
+      $('#temp-tags')
+        .append($('<li>')
+          .attr({id: 'repo-detail-tab'})
+          .append($('<a>')
+            .attr({href: hash})
+            .text('Repo Details')
+          )
+        );
+    } else if(type === 'repoissues') {
+      $('#temp-tags')
+        .append($('<li>')
+          .attr({id: 'repo-issues-tab'})
+          .append($('<a>')
+            .attr({href: hash})
+            .text('Repo Issues')
+          )
+        );
+    } else if(type === 'reponewissue'){
+      $('#temp-tags')
+        .append($('<li>')
+          .attr({id: 'repo-new-issue'})
+          .append($('<a>')
+            .attr({href: hash})
+            .text('New Issue')
+          )
+        );
+    } else {
+      return 'not a type';
+    }
+  };
+
+
+
 
   ns.init = function init() {
     ns.doNav(window.location.hash);
